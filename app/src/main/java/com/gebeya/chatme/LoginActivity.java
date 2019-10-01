7ce0226e89384f9aff2199f6a2a01d41f2b89f77
Melat Dagnachew <mel9dagnu@gmail.com>
2019-10-01 12:49:54 +0300

    Register,login,verification UI

--- app/src/main/java/com/gebeya/chatme/LoginActivity.java
+++ app/src/main/java/com/gebeya/chatme/LoginActivity.java
@@ -3,15 +3,25 @@ package com.gebeya.chatme;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText phoneNumberInput;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        phoneNumberInput= findViewById(R.id.phoneNumberInput);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void register_text(View view) {
@@ -20,7 +30,16 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void Login_button(View view) {
+
+        sendVerificationCode();
+
         Intent i = new Intent(this, VertificationActivity.class);
         startActivity(i);
+    }
+
+    private void sendVerificationCode(){
+
+        String phone = phoneNumberInput.getText().toString();
+
     }
 }
